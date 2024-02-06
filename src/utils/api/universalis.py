22fb0ac2979b1e8e6806f86520aee0cebfd842a2
Hugues Marandeau <h.marandeau@gmail.com>
2024-02-06 18:02:22 +0100

    Handle retries for Universalis API calls, 1 retry is usually enough but i want to be safe with default value of 2

--- src/utils/api/universalis.py
+++ src/utils/api/universalis.py
@@ -25,33 +25,39 @@ def get_universalis_response(items_id_to_name, server, timeframe_hours):
         for item_id in list(ids_chunk):
             ids_in_url_as_string += f"{str(item_id)},"
 
-        """
-            Under the hood, the logic that does the request starts by opening a socket to validate the connection.
-            This would sometimes hang until timeout, which took up to 2 mins.
-
-            After debugging, I noticed that this only happened when the socket opened using IPv6, and was always fine
-            when we did it using IPv4. It was flaky, sometimes worked for IPV6 and sometimes did not.
-
-            To avoid unecessary failures and spending more time debugging this issue which might be on the API's side,
-            I decided to mock the function opening the socket to force it to use IPv4.
-        """
-        with patch("socket.getaddrinfo", side_effect=getaddrinfoIPv4):
-            universalis_request = requests.get(
-                f"{UNIVERSALIS_REQUEST_URL}"
-                f"{server.value}/"
-                f"{ids_in_url_as_string}"
-                f"?entriesWithin={timeframe_hours * 60 * 60}",
-            )
-
-        if universalis_request.status_code != 200:
-            # TODO(): Handle retry if status_code is 5XX
-            print(
-                f"Failed to get requested items with error code {universalis_request.status_code}, "
-                f"{universalis_request.url}, "
-            )
-            pass
-        else:
-            result = json.loads(universalis_request.content)
-            combined_items_result = combined_items_result | result[ITEMS]
+        result = get_and_retry_universalis(
+            f"{UNIVERSALIS_REQUEST_URL}"
+            f"{server.value}/"
+            f"{ids_in_url_as_string}"
+            f"?entriesWithin={timeframe_hours * 60 * 60}",
+        )
+        combined_items_result = combined_items_result | result[ITEMS]
 
     return {ITEMS: combined_items_result}
+
+
+def get_and_retry_universalis(url, retries=2):
+    """
+    Under the hood, the logic that does the request starts by opening a socket to validate the connection.
+    This would sometimes hang until timeout, which took up to 2 mins.
+
+    After debugging, I noticed that this only happened when the socket opened using IPv6, and was always fine
+    when we did it using IPv4. It was flaky, sometimes worked for IPV6 and sometimes did not.
+
+    To avoid unecessary failures and spending more time debugging this issue which might be on the API's side,
+    I decided to mock the function opening the socket to force it to use IPv4.
+    """
+    for _ in range(0, retries):
+        with patch("socket.getaddrinfo", side_effect=getaddrinfoIPv4):
+            universalis_request = requests.get(url)
+
+        if universalis_request.status_code != 200:
+            pass
+        else:
+            return json.loads(universalis_request.content)
+
+    print(
+        f"Failed with error code {universalis_request.status_code} and {retries} retries to get url "
+        f"{universalis_request.url}"
+    )
+    return {}
